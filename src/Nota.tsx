70616830fcf4aafc174b01135466ffffcbe6420b
kiriasru <kiriasru@gmail.com>
2025-10-30 20:53:42 -0600

    Adding component

--- src/Nota.tsx
+++ src/Nota.tsx
@@ -5,8 +5,12 @@ const Nota: React.FC = () => {
 
 
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className="min-h-screen bg-gray-50 p-6 sm:p-10 font-sans">
+        <div className="max-w-4xl mx-auto">
+            <h1 className="text-3xl sm:text-5xl font-bold text-gray-900 text-center">
+                Calculadora de Notas Parciales
+            </h1>
+        </div>
     </div>
   )
 }
